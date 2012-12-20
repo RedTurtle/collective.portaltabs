6f35ffe977a7c3d9a80d7fdfdb23ea207f168d19
Luca Fabbri <luca@keul.it>
2012-12-20 15:23:08 +0100

    removed bad usage of setBaseLinecontext

--- collective/portaltabs/Extensions/install.py
+++ collective/portaltabs/Extensions/install.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
 def uninstall(portal, reinstall=False):
-    if not reinstall:
-        setup_tool = portal.portal_setup
-        setup_tool.setBaselineContext('profile-collective.portaltabs:uninstall')
-        setup_tool.runAllImportStepsFromProfile('profile-collective.portaltabs:uninstall')
-        try:
-            portal.portal_properties.manage_delObjects(ids=['portaltabs_settings'])
-        except AttributeError:
-            pass
+    setup_tool = portal.portal_setup
+    setup_tool.runAllImportStepsFromProfile('profile-collective.portaltabs:uninstall')
